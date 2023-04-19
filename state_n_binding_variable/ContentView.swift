38ba9a6173acf97f2730adec70a252aab2e86923
afitra <loeby45@gmail.com>
2023-04-19 15:18:43 +0700

    Initial Commit

---
+++ state_n_binding_variable/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  state_n_binding_variable
+//
+//  Created by afitra mamor on 19/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ state_n_binding_variable/state_n_binding_variableApp.swift
@@ -0,0 +1,17 @@
+//
+//  state_n_binding_variableApp.swift
+//  state_n_binding_variable
+//
+//  Created by afitra mamor on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct state_n_binding_variableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
